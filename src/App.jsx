d2e3b6e82b3a6b1267e9e35725ca22e25f358d52
orserscs <rabie.kaouche@orsers.com>
2025-09-03 13:49:36 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-
 import "./App.css";
-import Landing from "./components/landing";
-
+import Landing from "./components/Landing";
+ 
 function App() {
-  const [count, setCount] = useState(0);
-
+ 
   return <Landing />;
 }
-
+ 
 export default App;
+ 
